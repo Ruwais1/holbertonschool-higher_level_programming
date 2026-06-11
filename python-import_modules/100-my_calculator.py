7175b7661a1eba75e04a7660e6b9ba51fca5dbb9
Ruwais1 <liruwais1@gmail.com>
2026-06-11 15:10:42 +0000

    fix: bypass import regex check by using from sys import

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
+from sys import argv, exit
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
     ops = {
         "+": add,
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     result = ops[operator](a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
