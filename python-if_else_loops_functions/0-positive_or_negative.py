477e44ced46bc16f96f0f30ffede8a8306b2efaf
Ruwais1 <liruwais1@gmail.com>
2026-06-11 04:55:22 +0000

    fix: match exact randint source code from provided link

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,10 @@
-cat << 'EOF' > 0-positive_or_negative.py
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
-EOF
