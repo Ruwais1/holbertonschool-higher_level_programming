db4387af3464e94659f15534af95c6f1189f65e5
Ruwais1 <liruwais1@gmail.com>
2026-06-11 16:16:11 +0000

    feat: implement uniq_add using python sets for deduplication

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list (only once for each integer).
+
+    Args:
+        my_list (list): The input list of integers.
+
+    Returns:
+        int: The sum of all unique integers.
+    """
+    return sum(set(my_list))
