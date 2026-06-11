53a80a85965ee1af2a646d42a652726fe2e6cd01
Ruwais1 <liruwais1@gmail.com>
2026-06-11 18:25:50 +0000

    feat: implement print_square with full input validation and testing scopes

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+4-print_square module.
+
+This module provides a function `print_square` that prints a square
+using the `#` character based on a given size.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
