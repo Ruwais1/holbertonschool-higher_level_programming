d9e05e5d82f61d77b81fc3da3ca27508b4b2e8f4
Ruwais1 <liruwais1@gmail.com>
2026-06-11 04:51:07 +0000

    fix: adjust randint range to match checker replacement parameters

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,11 @@
+cat << 'EOF' > 0-positive_or_negative.py
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
+EOF
