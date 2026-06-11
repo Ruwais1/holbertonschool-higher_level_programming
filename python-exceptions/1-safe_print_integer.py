f416cdd32eec41f1938d2bb367024a989face4bd
Ruwais1 <liruwais1@gmail.com>
2026-06-11 17:25:20 +0000

    feat: implement safe_print_integer using duck typing and format exceptions

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Prints an integer with "{:d}".format().
+
+    Args:
+        value: The value to be printed, can be of any type.
+
+    Returns:
+        bool: True if value was correctly printed as an integer, False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
