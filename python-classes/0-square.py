8f2bc226f44cda55ee96965b21525845638b3440
Ruwais1 <liruwais1@gmail.com>
2026-06-18 18:35:50 +0300

    feat: create empty Square class with documentation

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-square module.
+
+This module provides a simple, empty class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square.
+    """
+    pass
