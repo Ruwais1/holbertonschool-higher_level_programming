c227462439c36d6b296e5b06b2c81ebd516d3a71
Ruwais1 <liruwais1@gmail.com>
2026-06-19 17:22:29 +0000

    fix: remove misplaced file

--- python-classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module defines an empty class Rectangle that represents a rectangle."""
-
-
-class Rectangle:
-    """An empty class that defines a geometric rectangle."""
-    pass
