769708dbc4f6ad76cfacaa71e6b1a0fd0b266935
Ruwais1 <liruwais1@gmail.com>
2026-06-19 17:23:44 +0000

    feat: define empty Rectangle class with strict docstrings

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines an empty class Rectangle that represents a rectangle."""
+
+
+class Rectangle:
+    """An empty class that defines a geometric rectangle."""
+    pass
