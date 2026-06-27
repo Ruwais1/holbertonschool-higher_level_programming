4a5c7880b3088a64f7c2e56898d9c332fcc9953e
Ruwais1 <liruwais1@gmail.com>
2026-06-27 21:10:56 +0000

    style: fix PEP8 line length in 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that writes a string to a text file and returns the number of characters.
+Module that writes a string to a text file and returns character count.
 """
 
 
