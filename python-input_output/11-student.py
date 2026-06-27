c37f0a80f7e91a53ea27c7746949cb9f73d079e1
Ruwais1 <liruwais1@gmail.com>
2026-06-27 21:53:07 +0000

    feat: add Student class with reload_from_json method

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that defines the class Student with JSON retrieval and reload.
+"""
+
+
+class Student:
+    """Class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only specific attributes are retrieved.
+        """
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
