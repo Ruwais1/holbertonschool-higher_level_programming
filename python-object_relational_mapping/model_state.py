c24868abc150f2269c8dcb15913b6e5bc577f180
Ruwais1 <liruwais1@gmail.com>
2026-07-24 19:37:20 +0000

    Fix PEP8 and docstrings in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Contains State class and Base instance to link to MySQL table states"""
+"""
+Module that contains the class definition of a State and an instance Base
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,8 +9,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class for MySQL states table"""
+    """
+    State class that inherits from Base and links to the MySQL table states
+    """
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
