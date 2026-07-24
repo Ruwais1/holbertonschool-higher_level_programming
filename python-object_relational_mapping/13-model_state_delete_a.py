c032b9d608994d84863299bc1627b2916ba18e78
Ruwais1 <liruwais1@gmail.com>
2026-07-24 19:39:39 +0000

    Fix PEP8 line length and import order in task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Deletes all State objects with a name containing the letter 'a' from the database"""
+"""
+Script that deletes all State objects with a name containing 'a'
+"""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db_uri, pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
