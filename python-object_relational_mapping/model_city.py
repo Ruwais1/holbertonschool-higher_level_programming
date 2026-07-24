2eb09ee99032bdcbe7e391e2ab7d86b6e9186f13
Ruwais1 <liruwais1@gmail.com>
2026-07-24 19:40:17 +0000

    Fix PEP8 and imports in task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Contains the class definition of a City"""
+"""
+Module that contains the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
 
 
 class City(Base):
-    """City class for MySQL cities table"""
+    """
+    City class that inherits from Base and links to the MySQL table cities
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
